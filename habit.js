2549c5d91bc442fb3ad8bf215359779ef481012e
Reeann Chloe Albert <rcalbertofc@gmail.com>
2023-07-03 21:00:13 -0400

    added ui function on theme button that toggles page from light to dark

--- habit.js
+++ habit.js
@@ -1,6 +1,28 @@
+//Query Selectors
 const habits = document.querySelectorAll('.habit-btn');
+const themeBtn = document.querySelector('#theme');
+
+//functions
+
+const storage = {
+
+}
+
+const ui = {
+    theme(){
+        themeBtn.classList.toggle('dark');
+        const root = document.querySelector(':root');
+        root.classList.toggle('dark');
+    }
+}
 
 
+//event listeners
+
+//event: theme button
+themeBtn.addEventListener('click', ui.theme);
+
+//delete
 habits.forEach(habit => {
     habit.addEventListener('click', () =>{
         habit.classList.toggle('completed');
